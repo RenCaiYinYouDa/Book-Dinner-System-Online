de473702352049c8ec70fcde09533fe58c0a4fe3
kenanmi <1225460869@qq.com>
2017-06-21 21:04:27 +0800

    新增内容
    1.dish类添加image字段
    2.添加实现

---
+++ BookDinnerSystemOnline/src/main/java/com/rcyyd/bookdinner/service/impl/CommentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.rcyyd.bookdinner.service.impl;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.rcyyd.bookdinner.service.CommentService;
+
+@Service
+@Transactional
+public class CommentServiceImpl implements CommentService {
+
+}
