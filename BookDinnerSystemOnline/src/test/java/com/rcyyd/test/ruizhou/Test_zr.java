1b904dc18e60b91c47e749040c83532b88ba617d
Fearain <1434340006@qq.com>
2017-06-21 11:13:30 +0800

    add method

--- BookDinnerSystemOnline/src/test/java/com/rcyyd/test/ruizhou/Test_zr.java
+++ BookDinnerSystemOnline/src/test/java/com/rcyyd/test/ruizhou/Test_zr.java
@@ -10,6 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.rcyyd.bookdinner.domain.ComUser;
 import com.rcyyd.bookdinner.persistence.ComUserDao;
 import com.rcyyd.bookdinner.persistence.impl.ComUserDaoImpl;
+import com.rcyyd.bookdinner.service.ComUserService;
 
 
 public class Test_zr {
@@ -30,14 +31,15 @@ private static ApplicationContext ctx = null;		// IoC容器(管理对象及对象依赖关系
 	
 	@Test
 	public void UserTest(){
+		ComUserService cs = (ComUserService)ctx.getBean(ComUserService.class);
 		ComUser user = new ComUser();
 		user.setUsername("aa");
 		user.setPassword("aa");
 		user.setEmail("aa");
 		user.setSecurity("aa");
 		user.setKey("aa");
-		ComUserDao dao = new ComUserDaoImpl();
-		dao.save(user);
+//		ComUserDao dao = new ComUserDaoImpl();
+		cs.register(user);
 		System.out.println("aa");
 	}
 	
