9f4a635325c9605e6824f47f3c6625b1c6270903
Fearain <1434340006@qq.com>
2017-06-22 16:56:45 +0800

    更改ComUserController，新增AdmUserController

--- BookDinnerSystemOnline/src/main/java/com/rcyyd/bookdinner/controller/AdminController.java
+++ BookDinnerSystemOnline/src/main/java/com/rcyyd/bookdinner/controller/AdminController.java
@@ -2,12 +2,14 @@ package com.rcyyd.bookdinner.controller;
 
 import javax.servlet.http.HttpSession;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.rcyyd.bookdinner.domain.AdmUser;
 import com.rcyyd.bookdinner.domain.Order;
 import com.rcyyd.bookdinner.domain.PageModel;
 import com.rcyyd.bookdinner.service.AdmUserService;
@@ -20,8 +22,6 @@ public class AdminController {
 	private OrderService orderService;
 	@Autowired
 	private DishService dishService;
-	@Autowired
-	private AdmUserService admUserService;
 	
 	@GetMapping("admin/showOrders")
 	public String toShowOrders(Integer page, Integer size, Model model){
@@ -38,26 +38,5 @@ public class AdminController {
 		return "admin/order-list";
 	}
 	
-	@PostMapping("/admlogin")
-	public String doLogin(String username, String password, String vc, Model model, HttpSession session) {
-		String code = (String) session.getAttribute("vc");
-		if (vc != null && code != null && vc.equalsIgnoreCase(code)) {
-			if (admUserService.login(username, password) != null) {
-				session.setAttribute("username", username);
-				return "redirect:show";
-			} else {
-				model.addAttribute("hint", "用户名或密码错误!");
-				return "login";
-			}
-		} else {
-			model.addAttribute("hint", "请输入正确的验证码!");
-			return "login";
-		}
-	}
 	
-	@PostMapping("/admlogout")
-	public String doLogout(HttpSession session) {
-		session.invalidate();
-		return "show";
-	}
 }
