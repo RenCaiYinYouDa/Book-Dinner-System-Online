e2fdbfb37f379c96d8ddbcb1c5027b888ccac46d
fatiry-tail09 <1447453323@qq.com>
2017-06-23 14:02:50 +0800

    AddressController

--- BookDinnerSystemOnline/src/main/java/com/rcyyd/bookdinner/controller/AddressController.java
+++ BookDinnerSystemOnline/src/main/java/com/rcyyd/bookdinner/controller/AddressController.java
@@ -1,10 +1,13 @@
 package com.rcyyd.bookdinner.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.rcyyd.bookdinner.domain.Address;
 import com.rcyyd.bookdinner.service.AddressService;
 import com.rcyyd.bookdinner.service.ComUserService;
 import com.rcyyd.bookdinner.service.UserService;
@@ -17,10 +20,18 @@ public class AddressController {
 	@Autowired
 	private ComUserService comUserService;
 	
-	@DeleteMapping("/address/{addrid}")
+	@DeleteMapping("/address")
 	@ResponseBody
-	public String delAddress(@PathVariable String address){
+	public String addAddress(Address address){
+		boolean flag = addressService.addAddress(address);
+		return flag ? "Success" : "Failed";
+	}
+	
+	@DeleteMapping("/address")
+	@ResponseBody
+	public String deleAddress(int addrid){
 		boolean flag = addressService.deleAddress(addrid);
 		return flag ? "Success" : "Failed";
 	}
+	
 }
