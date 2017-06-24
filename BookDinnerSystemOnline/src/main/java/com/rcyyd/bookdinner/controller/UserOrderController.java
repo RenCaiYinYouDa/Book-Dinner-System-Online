92604a4d3f4bd5eccaafbc06f7f17d9acfadd288
lufei8hai <305188769@qq.com>
2017-06-24 10:03:44 +0800

    修成错误

--- BookDinnerSystemOnline/src/main/java/com/rcyyd/bookdinner/controller/UserOrderController.java
+++ BookDinnerSystemOnline/src/main/java/com/rcyyd/bookdinner/controller/UserOrderController.java
@@ -3,10 +3,10 @@ package com.rcyyd.bookdinner.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rcyyd.bookdinner.domain.ComUser;
 import com.rcyyd.bookdinner.domain.Order;
